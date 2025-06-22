d20835e956921cb762f0028baaa3565caac3e5bb
Nirali Rajpura <niralirajpura@Mac.lan>
2025-06-22 18:05:41 -0500

    basic python

--- day1.py
+++ day1.py
@@ -1,10 +1,40 @@
 # this is single line comment 
 '''this is multiline comment'''
 
-# Example of Case sensitive 
-name = "tom"
-Name = "Jerry"
 
-print(name)
+
+# Example of Case sensitive 
+name = "tom" 
+Name = "Jerry" #  both the variables are different identifiers 
+print(name) 
 print(Name)
+
+
+
+#  Indentation:
+age = 32
+if age > 30:
+    print (age)
+
+
+
+# line continution 
+# use a ('/') to continue the statement to the next line. 
+total = 1+2+3+4+5+6+7+\
+4+5+6
+print (total)
+
+
+
+# multiple statement on a single line 
+x=5;y=5;z=x+y
+print (z)
+
+
+
+# semnatics in python 
+# variable assignment
+
+age=32
+type(age)
 
