9669c76c82b0a9650d3fe67bd7bdd0643949079a
Nirali Rajpura <niralirajpura@Mac.lan>
2025-06-29 17:30:47 -0500

    tuples.py

--- tuples.py
+++ tuples.py
@@ -4,10 +4,23 @@ print(empty_tuple)
 print(type(empty_tuple))
 
 print()
-# convert into list
-lst = list()
-print(type(lst))
 
-tpl =tuple()
-print(tpl)
-print(type(tpl))
+# convert list to tuple
+print("convert list to tuple")
+my_list = [10, 20, 30]
+print(type(my_list), my_list)
+
+my_tuple = tuple(my_list)
+print( type(my_tuple),my_tuple,)       
+
+print()
+
+# convert tuple to list
+print("convert tuple to list")
+
+my_tuple = (1, 2, 3, 4)
+print(my_tuple, type(my_tuple))
+
+my_list = list(my_tuple)
+print(my_list, type(my_list))
+        
