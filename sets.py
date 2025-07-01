2c597f962936d7d44a7dcf00e361919542b88a76
Nirali Rajpura <niralirajpura@Mac.lan>
2025-07-01 14:09:08 -0500

    added

--- sets.py
+++ sets.py
@@ -1,3 +1,11 @@
 # create a set
-my_set={1,2,3,4,5}
-print(my_set, type(set))
+my_set= {1,2,3,4,5}
+print(my_set, type(my_set))
+print()
+
+
+# empty set
+empty = set()
+print(empty, type(empty))
+
+# list inside set
